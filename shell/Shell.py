40ea399ac8f1b63f368aca4e9c2496fd4f12bc31
Armando Ortiz <mandoortiz6209@gmail.com>
2020-09-15 21:57:39 -0600

    updated comments

--- shell/Shell.py
+++ shell/Shell.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
+""""
+Armando Ortiz
+Program meant to replicate functionality of a shell
+"""
 import os, sys, re
 
-def handleInput(input):
+def handleInput(userInput):
+    """
+    used to manage input and pick between different shell possibilities
+    """
     args = userInput.split(' ')
     if userInput.lower() == 'exit':
         sys.exit(0)
@@ -15,19 +20,21 @@ def handleInput(input):
             os.write(1, (os.getcwd()+"\n").encode())
         except FileNotFoundError:
             os.write(1, ("Directory %s: No such file or directory\n" % args[1]).encode())
-            pass
     elif '/' in args[0][0]:
-        callExecve(args[0],args)
+        callExecve(args[0], args)
         os.write(2, ("Command %s No such command.\n" % args[0]).encode())
-        sys.exit(1)# terminate with error    
+        sys.exit(1)# terminate with error
 
     else:
         execute(args)
 
 def execute(args):
+    """
+    forks and attempts to execute program using callExecve()
+    """
     pid = os.getpid()
     rc = os.fork()
-    if rc < 0 :
+    if rc < 0:
         os.write(2, ("fork failed, returning %d\n" % rc).encode())
         sys.exit(1)
     elif rc == 0:
@@ -35,15 +42,18 @@ def execute(args):
             program = "%s/%s" % (dir, args[0])
             callExecve(program, args)
         os.write(2, ("Command %s not found. Try again.\n" % args[0]).encode())
-        sys.exit(1) # terminate with error    
+        sys.exit(1) # terminate with error
 
     else:
         childpid = os.wait()
 
-#Made to call execve for the programs that need it
+
 def callExecve(program, args):
+    """
+    made to call execve for the programs that need it
+    """
     try:
-        os.execve(program, args, os.environ)
+        os.execve(program, args, os.environ) # try to execute program
     except FileNotFoundError:
         pass
     except IndexError:
@@ -51,11 +61,11 @@ def callExecve(program, args):
     except Exception:
         sys.exit(1)
 
-# Main
+
 while True:
     if 'PS1' in os.environ:
         os.write(1, os.environ['PS1'].encode())
-    
+
     else:
         os.write(1, ("$ ").encode())
 
@@ -63,7 +73,6 @@ while True:
         userInput = input()
     except EOFError:
         sys.exit(1)
-        
+
     handleInput(userInput)
-    
     
